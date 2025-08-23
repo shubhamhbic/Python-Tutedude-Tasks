37ad7ceeb4573824754baf6538e49589d1814cee
shubhamhbic <145246375+shubhamhbic@users.noreply.github.com>
2025-08-23 10:32:38 +0530

    Create task1.py
    a simple calculator task1 complete

---
+++ Assignment1/task1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+print("Addition: ", sum)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("division: ", division)
