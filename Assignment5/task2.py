02af8ebd14e4b2f382fc19d6c9a50f76e78d09c5
shubhamhbic <145246375+shubhamhbic@users.noreply.github.com>
2025-10-03 10:20:36 +0530

    Create task2.py
    list slicing

---
+++ Assignment5/task2.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+print('Original list:', list)
+extracted = list[0:5]
+print('Extracted first five element:' ,extracted)
+extracted.reverse()
+print('Reversed extracted elements', extracted)
